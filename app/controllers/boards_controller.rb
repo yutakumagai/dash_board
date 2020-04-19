f3436893e464a81a15204d22b1e75996d3f5bd9b
yuta kumagai <nsywimperbass@gmail.com>
2020-04-20 00:50:47 +0900

    [fix] フィルター機能実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
     @boards = Board.all
   end
@@ -13,25 +15,21 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
 
-    redirect_to board
+    redirect_to @board
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.delete
+    @board.delete
 
-    redirect_to board_path
+    redirect_to boards_path
   end
 
   private
@@ -39,4 +37,9 @@ class BoardsController < ApplicationController
   def board_params
     params.require(:board).permit(:name, :title, :body)
   end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
+
 end
