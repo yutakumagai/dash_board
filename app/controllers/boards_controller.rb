268975f89cbf269b2625e86b698e94cb1dd2cd78
yuta kumagai <nsywimperbass@gmail.com>
2020-04-28 00:33:22 +0900

    [fix] 掲示板編集　修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -30,9 +30,14 @@ class BoardsController < ApplicationController
   end
 
   def update
-    @board.update(board_params)
-
-    redirect_to @board
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      redirect_to edit_board_path, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
   end
 
   def destroy
